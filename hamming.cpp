285d562b81f8ab5378a56bc2ee0e49111d287db0
Igor Engler <53021313+igorengler97@users.noreply.github.com>
2020-11-24 16:21:00 -0300

    Update hamming.cpp

--- hamming.cpp
+++ hamming.cpp
@@ -1,66 +1,69 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <string>
 
-int sindromeToDec(std::vector<int> novoC)
+int SindromeToDec(std::vector<int> novoC)
 {
     return(novoC.at(0)*1+novoC.at(1)*2+novoC.at(2)*4+novoC.at(3)*8+novoC.at(4)*16);
 }
 
-std::string reverseString(std::string inverter)
+std::string reverseString(std::string reverse)
 {
     for (int i = 0; i<21/2;i++)
     {
-        std::swap(inverter[i], inverter[21-i-1]);
+        std::swap(reverse[i], reverse[21-i-1]);
     }
-    return inverter;
+    return reverse;
 }
 
-void hamming(std::string entrada)
+std::string hamming(std::string input)
 {
     std::vector<int> novoC;
 
-    novoC.push_back(((entrada[18]-'0')^(entrada[16]-'0')^(entrada[14]-'0')^(entrada[11]-'0')^(entrada[10]-'0')^(entrada[8]-'0')^(entrada[6]-'0')^(entrada[4]-'0')^(entrada[2]-'0')^(entrada[0]-'0')^(entrada[20]-'0')));
-    novoC.push_back(((entrada[18]-'0')^(entrada[15]-'0')^(entrada[14]-'0')^(entrada[12]-'0')^(entrada[11]-'0')^(entrada[10]-'0')^(entrada[7]-'0')^(entrada[6]-'0')^(entrada[3]-'0')^(entrada[2]-'0')^(entrada[19]-'0')));
-    novoC.push_back(((entrada[16]-'0')^(entrada[15]-'0')^(entrada[14]-'0')^(entrada[9]-'0')^(entrada[8]-'0')^(entrada[7]-'0')^(entrada[6]-'0')^(entrada[1]-'0')^(entrada[0]-'0')^(entrada[17]-'0')));
-    novoC.push_back(((entrada[12]-'0')^(entrada[11]-'0')^(entrada[10]-'0')^(entrada[9]-'0')^(entrada[8]-'0')^(entrada[7]-'0')^(entrada[6]-'0')^(entrada[13]-'0')));
-    novoC.push_back(((entrada[4]-'0')^(entrada[3]-'0')^(entrada[2]-'0')^(entrada[1]-'0')^(entrada[0]-'0')^(entrada[5]-'0')));
+    novoC.push_back(((input[18]-'0')^(input[16]-'0')^(input[14]-'0')^(input[11]-'0')^(input[10]-'0')^(input[8]-'0')^(input[6]-'0')^(input[4]-'0')^(input[2]-'0')^(input[0]-'0')^(input[20]-'0')));
+    novoC.push_back(((input[18]-'0')^(input[15]-'0')^(input[14]-'0')^(input[12]-'0')^(input[11]-'0')^(input[10]-'0')^(input[7]-'0')^(input[6]-'0')^(input[3]-'0')^(input[2]-'0')^(input[19]-'0')));
+    novoC.push_back(((input[16]-'0')^(input[15]-'0')^(input[14]-'0')^(input[9]-'0')^(input[8]-'0')^(input[7]-'0')^(input[6]-'0')^(input[1]-'0')^(input[0]-'0')^(input[17]-'0')));
+    novoC.push_back(((input[12]-'0')^(input[11]-'0')^(input[10]-'0')^(input[9]-'0')^(input[8]-'0')^(input[7]-'0')^(input[6]-'0')^(input[13]-'0')));
+    novoC.push_back(((input[4]-'0')^(input[3]-'0')^(input[2]-'0')^(input[1]-'0')^(input[0]-'0')^(input[5]-'0')));
 
-    int cXor = sindromeToDec(novoC);
+    int cXor = SindromeToDec(novoC);
     if(cXor != 0){
-        std::string reversa = reverseString(entrada);
+        std::string reverse = reverseString(input);
         for (int i = 0; i<21; i++)
         {
             if(cXor-1 == i)
             {
-                if(reversa[i] == '1'){
-                    reversa[i] = '0';
+                std::cout<<"Error found on bit "<<cXor<<std::endl;
+                if(reverse[i] == '1'){
+                    reverse[i] = '0';
                 }
-                else if(reversa[i] == '0')
+                else if(reverse[i] == '0')
                 {
-                    reversa[i] = '1';
+                    reverse[i] = '1';
                 }
             }
-            entrada = reverseString(reversa);
+            input = reverseString(reverse);
         }
     }
-    std::string saida;
-    saida = std::string() + entrada[0]+entrada[1]+entrada[2]+entrada[3]+entrada[4]+entrada[6]+entrada[7]+entrada[8]+entrada[9]+entrada[10]+entrada[11]+entrada[12]+entrada[14]+entrada[15]+entrada[16]+entrada[18];
-    std::cout<<saida<<std::endl;
+    else{
+        std::cout<<"No error found"<<std::endl;
+    }
+    std::string output;
+    output = std::string() + input[0]+input[1]+input[2]+input[3]+input[4]+input[6]+input[7]+input[8]+input[9]+input[10]+input[11]+input[12]+input[14]+input[15]+input[16]+input[18];
+    return output;
 
 }
 
 int main()
 {
-    std::ifstream arquivo("entrada.txt");
-    std::string texto;
-    int linhas=0;
+    std::string input;
     int N = 0;
+    std::cout<<"Insert the amount of verifications to be done: ";
     std::cin>>N;
 
-    while(getline (arquivo, texto ) && N>0){
-        hamming(texto);
+    while(N>0){
+        std::cin>>input;
+        std::cout<<hamming(input)<<std::endl<<std::endl;
         N=N-1;
     }
 }
